e849a9013c0aa0acd48c0d0b1b9fd901265914e0
Naveed <64456140+Naveed233@users.noreply.github.com>
2025-01-27 18:37:49 +0900

    Update portfolio_app.py

--- portfolio_app.py
+++ portfolio_app.py
@@ -14,6 +14,7 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
+
 logger = logging.getLogger(__name__)
 
 # Set Streamlit page configuration
